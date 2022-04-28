8c5e8edcdb37d1f8b356e63dae8971da45b99bdd
WitherVictor <number42@yeah.net>
2022-04-28 14:51:23 +0800

    Iterators updated

--- include/icore/type/basic/numberArray.hpp
+++ include/icore/type/basic/numberArray.hpp
@@ -17,18 +17,26 @@
 #include "number.hpp"
 #include <iterator>
 #include <initializer_list>
+#include <exception>
 
 namespace i::core::type::basic
 {
-    template <typename Type>
+
+    template <typename Type, std::size_t Size>
     class numberArray_const_iterator
     {
-        using self = numberArray_const_iterator<Type>;
+    public:
+        using self = numberArray_const_iterator<Type, Size>;
         using element = number<Type>;
+
+
+        using value_type = Type;
+        using difference_type = ptrdiff_t;
         using pointer = const element*;
         using reference = const element&;
+        using iterator_concept = std::contiguous_iterator_tag;
+        using iterator_category = std::random_access_iterator_tag;
 
-    public:
         numberArray_const_iterator()
             : ptr(nullptr) {}
         numberArray_const_iterator(number<int>* iter)
@@ -67,14 +75,20 @@ namespace i::core::type::basic
         element* ptr;
     };
 
-    template <typename Type>
+    template <typename Type, std::size_t Size>
     class numberArray_iterator
     {
-        using self = numberArray_iterator<Type>;
+    public:
+        using self = numberArray_iterator<Type, Size>;
         using element = number<Type>;
+
+        using value_type = Type;
+        using difference_type = ptrdiff_t;
         using pointer = element*;
         using reference = element&;
-    public:
+        using iterator_concept = std::contiguous_iterator_tag;
+        using iterator_category = std::random_access_iterator_tag;
+
         numberArray_iterator(number<Type>* iter)
             : ptr(iter) {}
         
@@ -114,14 +128,18 @@ namespace i::core::type::basic
     template <typename Type, std::size_t Size>
     class numberArray
     {
-        using iterator = numberArray_iterator<Type>;
-        using const_iterator = numberArray_const_iterator<Type>;
-        using element = number<Type>;
     public:
+        using iterator = numberArray_iterator<Type, Size>;
+        using const_iterator = numberArray_const_iterator<Type, Size>;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+        using value_type = Type;
+        using element = number<Type>;
+        using pointer = element*;
+
         numberArray()
-            : _arr(new element[Size](0)) {}
+            : _arr({0}) {}
         numberArray(std::initializer_list<Type> init_list)
-            : _arr(new element[Size](0))
         {
             iterator iter = begin();
             auto list_iter = init_list.begin();
@@ -139,42 +157,82 @@ namespace i::core::type::basic
             }
         }
 
-        ~numberArray()
+        numberArray(const numberArray&) = default;
+
+        ~numberArray() = default;
+
+        element& at(std::size_t index)
         {
-            delete[] _arr;
-            _arr = nullptr;
+            if (!(index < size()))
+                throw std::out_of_range("Index is out of number array bound");
+            else
+                return _arr[index];
         }
 
-        iterator begin() { return iterator(_arr); }
-        iterator end() { return iterator(_arr + Size); }
-        const_iterator cbegin() { return const_iterator(_arr); }
-        const_iterator cend() { return const_iterator(_arr + Size); }
+        std::size_t size()
+        { return Size; }
+
+        element& operator[](std::size_t index)
+        { return _arr[index]; }
+
+        pointer data()
+        { return _arr; }
+
+        iterator begin()
+        { return iterator(_arr); }
+
+        iterator end()
+        { return iterator(_arr + Size); }
+
+        element& front()
+        { return *begin(); }
+
+        element& back()
+        { return *end(); }
+
+        const_iterator cbegin()
+        { return const_iterator(_arr); }
+
+        const_iterator cend()
+        { return const_iterator(_arr + Size); }
+
+        reverse_iterator rbegin()
+        { return reverse_iterator(end()); }
+
+        reverse_iterator rend()
+        { return reverse_iterator(begin()); }
+
+        const_reverse_iterator crbegin()
+        { return const_reverse_iterator(end()); }
+
+        const_reverse_iterator crend()
+        { return const_reverse_iterator(begin()); }
     private:
-        number<Type>* _arr;
+        number<Type> _arr[Size] {0};
     };
 }
 
 namespace std
 {
-    template <typename T>
-    using numberArray_iterator = i::core::type::basic::numberArray_iterator<T>;
+    template <typename T, std::size_t S>
+    using numberArray_iterator = i::core::type::basic::numberArray_iterator<T, S>;
     
-    template <typename T>
-    using numberArray_const_iterator = i::core::type::basic::numberArray_const_iterator<T>;
+    template <typename T, std::size_t S>
+    using numberArray_const_iterator = i::core::type::basic::numberArray_const_iterator<T, S>;
 
     template <typename T>
     using basic_number = i::core::type::basic::number<T>;
 
-    template <typename T>
-    numberArray_iterator<T> begin(const basic_number<T>& obj) { return obj.begin(); };
+    template <typename T, std::size_t S>
+    numberArray_iterator<T, S> begin(const basic_number<T>& obj) { return obj.begin(); };
 
-    template <typename T>
-    numberArray_iterator<T> end(const basic_number<T>& obj) { return obj.end(); };
+    template <typename T, std::size_t S>
+    numberArray_iterator<T, S> end(const basic_number<T>& obj) { return obj.end(); };
 
-    template <typename T>
-    numberArray_iterator<T> cbegin(const basic_number<T>& obj) { return obj.cbegin(); };
+    template <typename T, std::size_t S>
+    numberArray_const_iterator<T, S> cbegin(const basic_number<T>& obj) { return obj.cbegin(); };
 
-    template <typename T>
-    numberArray_iterator<T> cend(const basic_number<T>& obj) { return obj.cend(); };
+    template <typename T, std::size_t S>
+    numberArray_const_iterator<T, S> cend(const basic_number<T>& obj) { return obj.cend(); };
 
 }
